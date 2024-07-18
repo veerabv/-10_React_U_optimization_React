3fb79f72e77ff6c94cc8f5cc526c445f54fe191a
veerabv <bveera0608@gmail.com>
2024-07-18 14:24:01 +0530

    add useCallback

--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -1,8 +1,10 @@
 import { log } from '../../log.js';
 import { memo } from 'react';
 
+
+// here i use memo but the rerender happens because the value in ...props changed , you can see in the respective parent component to solve this we use useCallback
 const IconButton = memo(function IconButton({ children, icon, ...props }) {
-  log('<IconButton /> rendered', 2);
+  log('<IconButton /> rendered', 2);  
 
   const Icon = icon;
   return (
